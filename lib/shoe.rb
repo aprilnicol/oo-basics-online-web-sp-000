bc26e388688d2aab0ec5757a693230f44aa897a7
April Adams <illusionlace@gmail.com>
2020-09-18 16:56:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,12 +1,12 @@
 # Make your shoe class here!
-class Shoe 
+class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
   def initialize(brand)
-    @brand = brand 
-  end 
-  def brand 
-    @brand 
-  end 
-def cobble 
+    @brand = brand
+  end
+  def brand
+    @brand
+  end
+def cobble
   puts "Your shoe is as good as new!"
-end 
+end
